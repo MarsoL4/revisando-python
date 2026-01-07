7d560e757b3123c3dfee71a746d2f76a72d3ae0d
MarsoL4 <enzogmarsola@gmail.com>
2026-01-07 18:52:21 -0300

    adiciona a resolução do exercício de insertion sort

--- exercicios/ordenando_lista/insertion_sort.py
+++ exercicios/ordenando_lista/insertion_sort.py
@@ -8,3 +8,23 @@
 5. Teste sua função com pelo menos duas listas de números diferentes, incluindo uma já ordenada e uma em ordem reversa.
 
 Dica: pense em como “empurrar” os elementos à direita para liberar espaço para o número que está sendo inserido na posição correta."""
+
+import os
+os.system('cls')
+
+def insertion_sort(lista: list[int]) -> list:
+    # Passa por cada elemento da lista a partir do segundo
+    for i in range(1, len(lista)):
+        chave = lista[i]
+        j = i - 1
+        # Move os elementos da lista[0...i-1] que são maiores que a 'chave'
+        # uma posição à frente para criar espaço para a 'chave'
+        while j >= 0 and lista[j] > chave:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = chave
+    return lista
+
+print(insertion_sort([2, 1]))         # [1, 2]
+print(insertion_sort([3, 2, 1]))      # [1, 2, 3]
+print(insertion_sort([5, 3, 4, 1, 2]))# [1, 2, 3, 4, 5]
