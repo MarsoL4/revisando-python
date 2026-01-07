168c41c91c6d9830ad384f9cca0c0f5c48328464
MarsoL4 <enzogmarsola@gmail.com>
2026-01-06 22:25:54 -0300

    adiciona d_ antes do nome de cada exercício já feito

--- exercicios/basicos/d_even_numbers.py
+++ exercicios/basicos/d_even_numbers.py
@@ -4,7 +4,7 @@ Exercício 1: Imprima números pares
 1. Escreva um programa em Python que receba um número inteiro N do usuário.
 2. Imprima na tela todos os números pares de 0 até N, inclusive, um por linha.
 """
-from functions import recebe_numero
+from exercicios.functions import recebe_numero
 import os
 os.system('cls')
 
--- exercicios/basicos/d_multiplication_table.py
+++ exercicios/basicos/d_multiplication_table.py
@@ -4,7 +4,7 @@ Exercício 6: Tabuada personalizada
 1. Peça ao usuário para informar um número inteiro.
 2. Imprima a tabuada desse número de 1 a 10 (exemplo: 7 x 1 = 7 ... 7 x 10 = 70).
 """
-from functions import recebe_numero
+from exercicios.functions import recebe_numero
 import os
 os.system('cls')
 
