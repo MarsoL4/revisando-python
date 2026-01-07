31c310d63d4863cbf3034c1d4c68417ff37cfb0f
MarsoL4 <enzogmarsola@gmail.com>
2026-01-06 22:52:40 -0300

    adiciona a solução do exercicio de média

--- exercicios/estatistica/mean.py
+++ exercicios/estatistica/mean.py
@@ -6,3 +6,18 @@
 4. Teste sua função usando pelo menos três listas diferentes de números.
 
 Dica: não use bibliotecas externas (como numpy ou statistics) na primeira tentativa, para treinar a lógica do algoritmo."""
+
+def calcula_media(lista: list):
+    while True:
+        if lista != []:
+            media = sum(list(map(int, lista))) / len(lista)
+            if media == int(media):
+                return int(media)
+            else:
+                return media
+        else:
+            return 0
+
+print(f"Média da Lista [1, 2 , 3] = {calcula_media([1, 2, 3])}")    # -> 2
+print(f"Média da Lista [1, 2 , 4] = {calcula_media([1, 2, 4])}")    # -> 2,33333...
+print(f"Média da Lista [] = {calcula_media([])}")           # -> 0
