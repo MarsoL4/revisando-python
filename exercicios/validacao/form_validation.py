06c615f29f8a6c949a4646e5596275cb0d8fdf89
MarsoL4 <enzogmarsola@gmail.com>
2026-01-07 19:16:50 -0300

    adiciona a resolução do exercício de validação de formuláro

--- exercicios/validacao/form_validation.py
+++ exercicios/validacao/form_validation.py
@@ -1,6 +1,6 @@
 """Exercício 3: Validação Simples de Formulário
 
-1. Crie uma função chamada valida_formulario que recebe dois argumentos: nome (string) e idade (inteiro).
+1. Crie uma função chamada valida_formulario que recebe um dicionário como argumento contendo: nome (string) e idade (inteiro).
 2. Implemente as seguintes validações:
    a) O nome não pode ser vazio e deve ter pelo menos 3 caracteres.
    b) A idade deve ser um número inteiro maior que 0 e menor que 150.
@@ -8,3 +8,19 @@
 4. Teste sua função com diferentes combinações de valores válidos e inválidos, como nomes vazios, idade negativa, nomes curtos, etc.
 
 Dica: use condicionais (if/else) e funções como len() para analisar as entradas."""
+
+dados_1 = {'nome': 'Marcos', 'idade': 19}
+dados_2 = {'nome': 'Marcos', 'idade': 159}
+dados_3 = {'nome': '', 'idade': 25}
+
+def valida_form(dados_form: dict[str, int]):
+   if len(dados_form.get('nome', '')) > 3 and 0 < dados_form.get('idade', '') < 150:
+      print(f"Dados Recebidos = {dados_form} -> Válidos")
+      return True
+   else:
+      print(f"Dados Recebidos = {dados_form} -> Inválidos")
+      return False
+      
+valida_form(dados_1)
+valida_form(dados_2)
+valida_form(dados_3)
